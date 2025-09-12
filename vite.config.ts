cd24ca89feb8e0638fb9a63ccddc124ff4056020
patriotnewsactivism <patriotnewsactivism@gmail.com>
2025-09-11 19:57:55 -0500

    changes

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
